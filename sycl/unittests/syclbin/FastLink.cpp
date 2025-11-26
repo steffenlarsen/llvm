8f77765609c473872e87fc6ff25cdac6fd72051f
Larsen, Steffen <steffen.larsen@intel.com>
2025-11-26 03:47:00 -0800

    [SYCL][Unittests] Add SYCLBIN fast-linking unittests
    Signed-off-by: Larsen, Steffen <steffen.larsen@intel.com>

---
+++ sycl/unittests/syclbin/FastLink.cpp
@@ -0,0 +1,19 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include <sycl/sycl.hpp>
+
+#include <helpers/MockDeviceImage.hpp>
+#include <helpers/MockKernelInfo.hpp>
+#include <helpers/UrMock.hpp>
+
+#include <gtest/gtest.h>
+
+TEST(FastLink, Basic) {
+
+}
